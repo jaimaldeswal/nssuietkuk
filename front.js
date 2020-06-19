d6eb83f6a89f3afe1b7b7ee168428185a7b3a283
jaimal <jdeswal70@gmail.com>
2020-06-19 06:44:43 +0530

    first commit

---
+++ front.js
@@ -0,0 +1,34 @@
+//toggling color of navbar toggler
+$(".navbar-toggler").click(function(){
+    $(this).toggleClass("donee")
+})
+$(".navbar-toggler").mouseenter(function(){
+    $(this).toggleClass("donee")
+})
+$(".navbar-toggler").mouseleave(function(){
+    $(this).toggleClass("donee")
+})
+
+
+/* hover dropdown */
+$('.dropdown').hover(function(){ 
+    $('.dropdown-toggle', this).trigger('click'); 
+  });
+  $('.dropright').hover(function(){ 
+    $('.dropdown-toggle', this).trigger('click'); 
+  });
+  $('.dropleft').hover(function(){ 
+    $('.dropdown-toggle', this).trigger('click'); 
+  });
+
+
+
+
+  //++++++++++++++++++++++++++++++++++= testing ++++++++++++++++++++++++++++++++++++++
+  // $(".jaimal").css({
+  //   background:"blue"
+  // })
+
+  //$("h1").text("jquerry");
+
+  
